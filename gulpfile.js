5ed5c15fecb5d68f97f62c9cb560aa4f79f5dd9e
Kruchy <karol@krusiec.com>
2017-02-05 23:26:34 +0100

    added site template

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create(); //to dodane ostatnio
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+  return gulp.src('scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      outputStyle: 'nested'
+      //outputStyle: 'expanded'
+      //outputStyle: 'compact'
+      //outputStyle: 'compressed'
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('css'))
+})
+
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: "./"
+});
+
+  gulp.watch('scss/**/*.scss', ['sass']);
+  gulp.watch('css/**/*.css').on('change', browserSync.reload);
+  gulp.watch('*.html').on('change', browserSync.reload);
+  gulp.watch('js/**/*.js').on('change', browserSync.reload);
+})
+
+gulp.task('default', ['serve']);
