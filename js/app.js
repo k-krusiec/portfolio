925d74fbb90f62e3d86b8d659ec9e0e35eb4a37f
k-krusiec <karol@krusiec.com>
2017-02-09 12:00:40 +0100

    updated header change

--- js/app.js
+++ js/app.js
@@ -4,16 +4,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function changeMenuOnScroll() {
     // var menuLogo = document.querySelector('.menu-logo');
+    var header = document.querySelector('.main-nav')
     var headerLogo = document.querySelector('.h-logo');
     var headerLogotxt = document.querySelector('.h-txt');
 
     window.addEventListener('scroll', function(e) {
       var top = this.scrollY;
       if (top >= 136) {
+        header.classList.add('scrolled');
         menuLogo.classList.remove('hidden');
         headerLogo.classList.add('hidden');
         headerLogotxt.classList.add('hidden');
       } else {
+        header.classList.remove('scrolled');
         menuLogo.classList.add('hidden');
         headerLogo.classList.remove('hidden');
         headerLogotxt.classList.remove('hidden');
