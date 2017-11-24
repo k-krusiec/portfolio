567ee45cd253ca5ea7d0b07fd674842f35edd8a5
Karol Krusiec <karol@krusiec.com>
2017-11-24 14:07:25 +0100

    webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,58 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: './src/index.js'
+    app: './src/index.js'
   },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['./dist']),
+    new HtmlWebpackPlugin({
+      title: 'Karol Krusiec - Portfolio',
+      template: './src/views/index.html',
+      chunks: 'app'
+    })
+  ],
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: "style-loader"
+      }, {
+        loader: "css-loader"
+      }, {
+        loader: "sass-loader"
+      }]
+    }, {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          outputPath: './images/',
+          publicPath: '../images'
+        }
+      }]
+    }, {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          outputPath: './fonts/',
+          publicPath: '../fonts'
+        }
+      }]
+    }]
   }
 };
